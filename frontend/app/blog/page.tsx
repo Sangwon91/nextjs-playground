5c44b9ba3e548787efe7c6c7eb79f1ec7409414d
Sangwon <lsw91@lgchem.com>
2025-10-21 07:29:43 +0000

    Add BlogPage component with sample posts and image support; integrate inter font and update layout

--- frontend/app/blog/page.tsx
+++ frontend/app/blog/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import clsx from 'clsx';
-import { inter } from '@/app/ui/fonts';
+import { inter } from '../ui/fonts';
 import Image from 'next/image';
 
 
