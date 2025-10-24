c10675f6626ec5cfe6945beab28a4155ac5d61c3
Sangwon <lsw91@lgchem.com>
2025-10-24 07:31:25 +0000

    feat: add MDX support with KaTeX and Pretty Code
    - Created a dynamic page for rendering MDX content based on slug.
    - Added a welcome MDX file with various content types including math and code blocks.
    - Implemented MDX components for custom rendering.
    - Updated Next.js configuration to support MDX with remark and rehype plugins for math and syntax highlighting.
    - Added necessary dependencies for KaTeX, rehype-pretty-code, and remark-math.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,23 @@
 import type { NextConfig } from "next";
 import createMDX from '@next/mdx';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeKatex from "rehype-katex";
+import remarkMath from 'remark-math';
 
 
 const nextConfig: NextConfig = {
   /* config options here */
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
 };
+
 
 // withMDX 래퍼 생성
 const withMDX = createMDX({
   // 필요한 경우 여기에 마크다운 플러그인 추가
+  options: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypePrettyCode, rehypeKatex],
+  },
 });
 
 // MDX 설정과 Next.js 설정을 병합
